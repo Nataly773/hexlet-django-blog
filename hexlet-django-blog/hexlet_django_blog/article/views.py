b4b5148ed23c07e212f3d1107429bd62631d3b72
Нечаева Наталья <miss.nechaewa@gmail.com>
2025-08-02 15:15:59 +0300

    add .

--- hexlet-django-blog/hexlet_django_blog/article/views.py
+++ hexlet-django-blog/hexlet_django_blog/article/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("article")
+    context = {
+        'title': 'Articles',
+        'description': 'This is the articles index page.',
+    }
+    return render(request, 'articles/index.html', context)
